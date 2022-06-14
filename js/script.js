defd6c889822ab56d9bc0a779b4fa305329c8acf
Erison Santos <erison.santos@ymail.com>
2022-06-14 16:28:57 -0400

    Add de elementos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+const clouds = document.querySelector('.clouds');
+const wallpaper = document.querySelector('.wallpaper')
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const coin = document.querySelector('.coin');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -11,18 +14,31 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
+    const wallpaperPosition = wallpaper.offsetLeft;    
+    const cloudsPosition = clouds.offsetLeft;
     const pipePosition = pipe.offsetLeft;
+    const coinPosition = coin.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPosition);
-    
+    /* console.log(marioPosition); */
+    /* console.log(coinPosition); */
+        
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        coin.style.animation = 'none';
+        coin.style.left = `${coinPosition}px`;
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
+
+        wallpaper.style.animation = 'none';
+        wallpaper.style.left = `${wallpaperPosition}px`;
 
         mario.src = './images/game-over.png';
         mario.style.width = '75px'
